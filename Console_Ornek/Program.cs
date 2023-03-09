3d1602c0d05d9502e33e1bdd5cb63f697b420e75
m.sinans <msomercikoglu@gmail.com>
2023-03-09 14:38:49 +0300

    Add project files.

---
+++ Console_Ornek/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+            Console.Write("Merhaba");
+            Console.WriteLine(" Gençler!");
+            string kelime;
+            kelime = "Hallo!";
+            Console.WriteLine(kelime);
+            int sayi = 11;
+            Console.Write(sayi);
+            Console.Read();
+        }
+    }
+}
